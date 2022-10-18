03e8af1ccfa6f351fd23c695cffed0f66c708c64
Benny <devbenny@gsitm.com>
2022-10-18 15:49:49 +0900

    🧪 :: @Query를 이용한 상품 조회 테스트

--- shop/shop/src/main/java/com/shop/repository/ItemRepository.java
+++ shop/shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail")String itemDetail);
 }
--- shop/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -88,4 +88,14 @@ class ItemRepositoryTest {
             System.out.println(item.toString());
         }
     }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세설명");
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 }

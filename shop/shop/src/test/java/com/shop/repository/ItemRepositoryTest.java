cf23fb952d6ec97921641082e2ce9026f5362a75
Benny <devbenny@gsitm.com>
2022-10-18 15:40:22 +0900

    🧪 :: 상품명, 상품 상세설명 OR 테스트

--- shop/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -20,6 +20,21 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
+
     public void createItemList(){
         for(int i=1; i<=10; i++){
             Item item = new Item();
@@ -39,6 +54,16 @@ class ItemRepositoryTest {
     public void findByItemNmTest(){
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명 5");
         for (Item item : itemList){
             System.out.println(item.toString());
         }

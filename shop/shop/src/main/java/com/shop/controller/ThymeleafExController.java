a97a2b593ffa734189c2e83b90db96cf99c0cc75
Benny <devbenny@gsitm.com>
2022-10-19 10:31:41 +0900

    📝 :: th:if, th:unless 예제

--- shop/shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -47,4 +47,20 @@ public class ThymeleafExController {
         model.addAttribute("itemDtoList", itemDtoList);
         return "thymeleafEx/thymeleafEx03";
     }
+
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
 }

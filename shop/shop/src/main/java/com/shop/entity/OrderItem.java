7baf2316cf5fe3bf7644cb0964b46ee74eab39e5
Benny <devbenny@gsitm.com>
2022-10-19 16:10:43 +0900

    Revert "⚡ :: 지연 로딩 방식 설정"
    This reverts commit 96ac7a21635552efc6ad7a26610ab62c84126b2a.

--- shop/shop/src/main/java/com/shop/entity/OrderItem.java
+++ shop/shop/src/main/java/com/shop/entity/OrderItem.java
@@ -15,11 +15,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 

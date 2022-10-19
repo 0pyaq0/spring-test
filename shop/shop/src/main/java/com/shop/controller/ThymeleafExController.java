4e9afc8fffb192ad22a0b3c49afa941a47933a26
Benny <devbenny@gsitm.com>
2022-10-19 10:27:56 +0900

    📝 :: th:each 예제

--- shop/shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -30,4 +32,19 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx02";
     }
 
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
 }

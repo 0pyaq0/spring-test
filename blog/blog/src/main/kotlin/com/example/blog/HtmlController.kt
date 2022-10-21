ed1af171f1c94d228e54e174b373a1d2a3957ef3
Benny <devbenny@gsitm.com>
2022-10-21 10:46:18 +0900

    📑 :: mustache 생성

--- blog/blog/src/main/kotlin/com/example/blog/HtmlController.kt
+++ blog/blog/src/main/kotlin/com/example/blog/HtmlController.kt
@@ -10,7 +10,7 @@ class HtmlController {
 
     @GetMapping("/")
     fun blog(model: Model): String {
-        model["title"] = "Blog"
+        model["title"] = "Blog" //model.addAttribute("title", "Blog")
         return "blog"
     }
 }

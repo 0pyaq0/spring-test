f93db6c430a80ae9c712a5dc10aff03de5b3a078
Benny <devbenny@gsitm.com>
2022-10-24 15:50:24 +0900

    ⚡ :: axios 사용, select 기능

--- todolist-frontend/src/App.js
+++ todolist-frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const baseUrl = "http://localhost:8080"
+
+  useEffect(()=> {
+    getTodos();
+  }, []);
+
+  async function getTodos(){
+    await axios
+    .get(baseUrl+"/todo")
+    .then((response)=>{
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }

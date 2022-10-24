e5aa928210f4259b1cbd5247dffa409034bd0093
Benny <devbenny@gsitm.com>
2022-10-24 15:42:08 +0900

    ⚙ :: todolist-frontend 프로젝트 생성

--- todolist/src/main/kotlin/com/example/todolist/controller/TodoController.kt
+++ todolist/src/main/kotlin/com/example/todolist/controller/TodoController.kt
@@ -24,6 +24,6 @@ class TodoController(
     @PutMapping(path = ["/{todoId}"])
     fun updateTodo(@PathVariable("todoId") todoId: Long) = todoService.updateTodo(todoId)
 
-    @DeleteMapping(path = ["/{todoId"])
+    @DeleteMapping(path = ["/{todoId}"])
     fun deleteTodo(@PathVariable("todoId") todoId: Long) = todoService.deleteTodo(todoId)
 }

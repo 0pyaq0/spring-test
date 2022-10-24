16d5cc4796c29a3f5f15bc84677879b592b26b78
Benny <devbenny@gsitm.com>
2022-10-24 15:55:13 +0900

    📑 :: Spring Core 설정

---
+++ todolist/src/main/kotlin/com/example/todolist/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package com.example.todolist.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name,
+                        HttpMethod.POST.name,
+                        HttpMethod.PUT.name,
+                        HttpMethod.DELETE.name,
+                )
+    }
+}

0c164afb8331968d2cea678b7c6b8278ee0f7229
Benny <devbenny@gsitm.com>
2022-10-24 16:15:59 +0900

    📑 :: Todo UI

--- todolist-frontend/src/App.js
+++ todolist-frontend/src/App.js
@@ -1,9 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const baseUrl = "http://localhost:8080"
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(()=> {
     getTodos();
@@ -13,16 +16,44 @@ function App() {
     await axios
     .get(baseUrl+"/todo")
     .then((response)=>{
-      console.log(response.data)
+      setTodos(response.data)
     })
     .catch((error) => {
       console.error(error)
     })
   }
 
+  function changeText(e){
+    e.preventDefault();
+    setInput(e.target.value)
+  }
+
   return (
     <div className="App">
-      hello
+      <h1>TODO LIST</h1>
+      <form>
+        <label>
+        Todo &nbsp;
+        <input type="text" required={true} value={input} onChange={changeText}/>
+        </label>
+        <input type="submit" value="create"/>
+      </form>
+
+      {
+        todos
+        ? todos.map((todo)=>{
+          return(
+            <div className="todo">
+              <h3>
+                <label onClick={null}>
+                  {todo.todoName}
+                </label>
+              </h3>
+            </div>
+          )
+        }) 
+        : null
+      }
     </div>
   );
 }

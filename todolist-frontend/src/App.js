0e2f569add5405356603aa606e13da3a2e86fb55
Benny <devbenny@gsitm.com>
2022-10-25 13:16:34 +0900

    for review

---
+++ todolist-frontend/src/App.js
@@ -0,0 +1,120 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const baseUrl = "http://localhost:8080"
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(()=> {
+    getTodos();
+  }, []);
+
+  async function getTodos(){
+    await axios
+    .get(baseUrl+"/todo")
+    .then((response)=>{
+      setTodos(response.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  function insertTodo(e){
+    e.preventDefault();
+
+    const insertTodo = async() => {
+      await axios
+            .post(baseUrl + "/todo", {
+              todoName:input
+            })
+            .then((response) => {
+              console.log(response.data)
+              setInput("");
+              getTodos();
+            })
+            .catch((error)=>{
+              console.error(error);
+            })
+    }
+    insertTodo();
+    console.log("할일 추가되었음")
+  }
+
+  function updateTodo(id) {
+    const updateTodo = async () => {
+      await axios
+        .put(baseUrl + "/todo/" + id, {})
+        .then((response) => {
+          setTodos(
+            todos.map((todo) =>
+              todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+          );
+          console.log(todos);
+        })
+        .catch((error) => {
+          console.error(error);
+        });  
+    }
+    updateTodo();
+  }
+
+  function deleteTodo(id) {
+    const deleteTodo = async () => {
+      await axios
+            .delete(baseUrl + "/todo/" + id, {})
+            .then((response) => {
+              setTodos(
+                todos.filter((todo) => todo.id !== id)
+              )
+            })
+            .catch((error) => {
+              console.error(error);
+            })
+    }
+    deleteTodo();
+  }
+
+  function changeText(e){
+    e.preventDefault();
+    setInput(e.target.value)
+  }
+
+  return (
+    <div className="App">
+      <h1>TODO LIST</h1>
+      <form onSubmit={insertTodo}>
+        <label>
+        Todo &nbsp;
+        <input type="text" required={true} value={input} onChange={changeText}/>
+        </label>
+        <input type="submit" value="create"/>
+      </form>
+
+      {
+        todos
+        ? todos.map((todo)=>{
+          return(
+            <div className="todo" key={todo.id}>
+              <h3>
+                <label 
+                className={todo.completed ? "completed" : null}
+                onClick={() => updateTodo(todo.id)}>
+                  {todo.todoName}
+                </label>
+                <label onClick={() => deleteTodo(todo.id)}>&nbsp;&nbsp;&nbsp;❌</label>
+              </h3>
+            </div>
+          )
+        }) 
+        : null
+      }
+    </div>
+  );
+}
+
+export default App;
